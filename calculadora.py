35408e2ab40f0053c3bbf742efb9bab8c1d9c0d1
alpha.ﾠ <36175929+alphafofo@users.noreply.github.com>
2022-04-23 15:51:05 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,74 @@
+
+escolha = int(input("Digite o tipo de tabuada que você \n1- ADIÇÃO \n2- SUBTRAÇÃO \n3- MULTIPLICAÇÃO \n4- DIVISÃO \nDigite uma opção: "))
+
+if escolha == 1:
+    
+         adicao = int(input("Digite o numero da tabuada a qual você quer obter resposta: "))
+
+         print("================")
+         print(adicao, " + 1 = ", adicao + 1)
+         print(adicao, " + 2 = ", adicao + 2)
+         print(adicao, " + 3 = ", adicao + 3)
+         print(adicao, " + 4 = ", adicao + 4)
+         print(adicao, " + 5 = ", adicao + 5)
+         print(adicao, " + 6 = ", adicao + 6)
+         print(adicao, " + 7 = ", adicao + 7)
+         print(adicao, " + 8 = ", adicao + 8)
+         print(adicao, " + 9 = ", adicao + 9)
+         print(adicao, " + 10 = ", adicao + 10)
+         print("================")
+         exit()
+
+if escolha == 2:
+        subtracao = int(input("Digite o numero da tabuada a qual você quer obter resposta: "))
+
+        print("================")
+        print(subtracao, " - 1 = ", subtracao - 1)
+        print(subtracao, " - 2 = ", subtracao - 2)
+        print(subtracao, " - 3 = ", subtracao - 3)
+        print(subtracao, " - 4 = ", subtracao - 4)
+        print(subtracao, " - 5 = ", subtracao - 5)
+        print(subtracao, " - 6 = ", subtracao - 6)
+        print(subtracao, " - 7 = ", subtracao - 7)
+        print(subtracao, " - 8 = ", subtracao - 8)
+        print(subtracao, " - 9 = ", subtracao - 9)
+        print(subtracao, " - 10 = ", subtracao - 10)
+        print("================")
+        exit()
+
+if escolha == 3:
+        multiplicacao = int(input("Digite o numero da tabuada a qual você quer obter resposta: "))
+
+        print("================")
+        print(multiplicacao, " x 1 = ", multiplicacao * 1)
+        print(multiplicacao, " x 2 = ", multiplicacao * 2)
+        print(multiplicacao, " x 3 = ", multiplicacao * 3)
+        print(multiplicacao, " x 4 = ", multiplicacao * 4)
+        print(multiplicacao, " x 5 = ", multiplicacao * 5)
+        print(multiplicacao, " x 6 = ", multiplicacao * 6)
+        print(multiplicacao, " x 7 = ", multiplicacao * 7)
+        print(multiplicacao, " x 8 = ", multiplicacao * 8)
+        print(multiplicacao, " x 9 = ", multiplicacao * 9)
+        print(multiplicacao, " x 10 = ", multiplicacao * 10)
+        print("================")
+        exit()
+
+if escolha == 4:
+        divisao = int(input("Digite o numero da tabuada a qual você obter resposta: "))
+
+        print("================")
+        print(divisao, " / 1 = ", divisao / 1)
+        print(divisao, " / 2 = ", divisao / 2)
+        print(divisao, " / 3 = ", divisao / 3)
+        print(divisao, " / 4 = ", divisao / 4)
+        print(divisao, " / 5 = ", divisao / 5)
+        print(divisao, " / 6 = ", divisao / 6)
+        print(divisao, " / 7 = ", divisao / 7)
+        print(divisao, " / 8 = ", divisao / 8)
+        print(divisao, " / 9 = ", divisao / 9)
+        print(divisao, " / 10 = ", divisao / 10)
+        print("================")
+        exit()
+else:
+        print("Opção invalida programa finalizado")
+        exit()
